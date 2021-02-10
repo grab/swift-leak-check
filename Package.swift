a65212337318eeb1ff0a4a0262b4133b871592ac
Pham Hoang Le <hoang.le@grabtaxi.com>
2021-02-10 17:46:06 +0800

    Change minimum version of Swift toolchain to 5.1

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 //
 //  Copyright 2020 Grabtaxi Holdings PTE LTE (GRAB), All rights reserved.
@@ -16,7 +16,12 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-      .package(name: "SwiftSyntax", url: "https://github.com/apple/swift-syntax", .exact("0.50300.0")),
+
+      .package(url: "https://github.com/apple/swift-syntax", .exact("0.50300.0")),      
+
+      // For Swift toolchain 5.2 and above
+      // .package(name: "SwiftSyntax", url: "https://github.com/apple/swift-syntax", .exact("0.50300.0")),
+      
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
